6772ad9df1d2ef38fdc24801461d5f93623033e4
Nicolas pereira <90424448+senhorN@users.noreply.github.com>
2022-10-17 13:48:34 -0300

    internal files

---
+++ ContadorRegressivo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorRegressivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade rec = new Recursividade();
+
+            ////variavel valor criada
+            int valor;
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+            Console.WriteLine("==              Contador regressivo           == ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+
+
+
+            //O usuário informa um valor
+            Console.WriteLine(DateTime.Now.ToString("dd/MMM/yyyy HH:mm:SS") + ("| Informe um número:"));
+            valor = int.Parse(Console.ReadLine());
+
+            //pulo de uma linha 
+            Console.WriteLine();
+
+            //recebe valor do usuário 
+            int obj = rec.Factorial(valor);
+            Console.WriteLine(obj);
+
+            
+
+            
+        
+    
+        }
+    } 
+}
---
+++ ContadorRegressivo/Recursividade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContadorRegressivo
+{
+	public class Recursividade
+	{
+		//função recurssiva para calcular o fatorial de n
+		
+		public int Factorial(int pValor)
+		{
+			
+
+			if (pValor <= 1)
+			{
+                Console.WriteLine(pValor);
+                if (pValor == 1)
+                {
+                    Console.WriteLine("Explosão!!!");
+                }
+				return 1;
+			}
+			else
+			{
+                Console.WriteLine(pValor);
+				return Factorial(pValor - 1) * pValor;
+
+			}
+			
+		}
+
+
+		
+		
+	}
+}
